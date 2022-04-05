c739c0fd7833af578ac9a087aa725aafe96e9b0e
Maksim Shatokhin <max.sh99@mail.ru>
2022-04-06 01:19:15 +0300

    rename my_user_id models fields to user_id

--- backend/user_rating/models.py
+++ backend/user_rating/models.py
@@ -24,14 +24,14 @@ class UserQuestion(models.Model):
     Поле has_an_answer принимает значение True, если пользователь, задавший вопрос, отметил одно из следующих за ним
     сообщений как правильный ответ
     """
-    my_user_id = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     question = models.CharField(max_length=1024)
     likes_num = models.IntegerField(default=0)
     dislikes_num = models.IntegerField(default=0)
     has_an_answer = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.my_user_id=} {self.question=} {self.has_an_answer=} {self.likes_num=} {self.dislikes_num=}'
+        return f'{self.user_id=} {self.question=} {self.has_an_answer=} {self.likes_num=} {self.dislikes_num=}'
 
 
 class UserAnswer(models.Model):
@@ -40,11 +40,11 @@ class UserAnswer(models.Model):
     Поле best_answer принимает значение True, если пользователь, задавший вопрос, или админ
     отметил этот ответ как правильный
     """
-    my_user_id = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(MyUser, on_delete=models.CASCADE)
     answer = models.CharField(max_length=1024)
     likes_num = models.IntegerField(default=0)
     dislikes_num = models.IntegerField(default=0)
     best_answer = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.my_user_id=} {self.likes_num=} {self.dislikes_num=} {self.best_answer=}'
+        return f'{self.user_id=} {self.likes_num=} {self.dislikes_num=} {self.best_answer=}'
